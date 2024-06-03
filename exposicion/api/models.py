ab7622f06160ef8c411a656cb89fc89be345c909
Sebastian Ilbay <atlantiscityplay@gmail.com>
2024-06-03 15:27:45 -0500

    sobreescritura de metodos
    sobreescritura de metodos

--- exposicion/api/models.py
+++ exposicion/api/models.py
@@ -4,16 +4,20 @@ class Bodega(models.Model):
     bod_id = models.AutoField(primary_key=True)
     bod_nombre = models.CharField(max_length=30)
     bod_ubicacion = models.CharField(max_length=30)
+    bod_eliminado = models.CharField(max_length=2,default='no')
 
 class Articulo(models.Model):
     art_id=models.AutoField(primary_key=True)
     art_nombre = models.CharField(max_length=30)
+    art_eliminado = models.CharField(max_length=2,default='no')
 
 class DetalleBodega(models.Model):
     det_bod_id = models.AutoField(primary_key=True)
-    det_bod_bod = models.ForeignKey(Bodega, on_delete=models.CASCADE)
-    det_bod_art = models.ForeignKey(Articulo, on_delete=models.CASCADE)
+    det_bod_bod = models.ForeignKey(Bodega, on_delete=models.DO_NOTHING, null=True) #models.setNull
+    det_bod_art = models.ForeignKey(Articulo, on_delete=models.DO_NOTHING, null=True)
     det_bod_cantidad = models.PositiveIntegerField()
+    det_bod_eliminado = models.CharField(max_length=2,default='no')
+
 
     #class Meta:
         #managed = False
--- exposicion/api/views.py
+++ exposicion/api/views.py
@@ -2,10 +2,38 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import *
 from .serializer import *
+from rest_framework.response import Response
+from rest_framework import status
+
 
 class BodegaViewSet(viewsets.ModelViewSet):
     queryset = Bodega.objects.all()
     serializer_class = BodegaSerializer
+
+    # sobreescritura get
+    def get_queryset(self):
+        return Bodega.objects.filter(bod_eliminado="no")
+
+    # sobreescritura delete
+    def destroy(self, request, *args, **kwargs):
+        id = kwargs.get('pk')
+        try:
+            bodega = Bodega.objects.get(bod_id=id)
+            bodega.bod_eliminado = "si"
+            bodega.save()
+
+            detalleBodega = DetalleBodega.objects.filter(det_bod_bod_id=id)
+            detalleBodega.update(det_bod_eliminado = "si")
+            
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Bodega.DoesNotExist:
+            return Response({'error': 'Bodega no encontrada'}, status=status.HTTP_404_NOT_FOUND)
+    
+    #retrive(self, request, *args, **kwargs)
+    #perform_create(self, serializer)
+    #update(self, request, *args, **kwargs):
+    #
+
 
 class ArticuloViewSet(viewsets.ModelViewSet):
     queryset = Articulo.objects.all()
@@ -14,3 +42,9 @@ class ArticuloViewSet(viewsets.ModelViewSet):
 class DetalleBodegaViewSet(viewsets.ModelViewSet):
     queryset = DetalleBodega.objects.all()
     serializer_class = DetalleBodegaSerializer
+
+    # sobreescritura get
+    def get_queryset(self):
+        return DetalleBodega.objects.filter(det_bod_eliminado="no")
+
+    
